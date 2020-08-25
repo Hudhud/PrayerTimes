be25bfe7a5bab5c58004c7ba4d485e2810e05e10
Hadi Horani <hadihorani@hotmail.com>
2020-08-25 19:00:41 +0200

    cahnged storing data from a single user session to storing the data globally

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,11 +2,9 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using PrayerTimes.Models;
 
@@ -14,54 +12,47 @@ namespace PrayerTimes.Controllers
 {
     public class HomeController : Controller
     {
-
         static string selected_City;
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public async Task<IActionResult> Index()
         {
-            string ctx, muwaqqit_URL, sessionString;
-            var ctx_cph = HttpContext.Session.GetString("PrayerData_cph");
-            var ctx_odense = HttpContext.Session.GetString("PrayerData_odense");
-            var ctx_aarhus = HttpContext.Session.GetString("PrayerData_aarhus");
-            var ctx_aalborg = HttpContext.Session.GetString("PrayerData_aalborg");
-
-            switch (selected_City)
+            if (Database.api == null)
+                Database.api = new System.Collections.Generic.List<APIResult>();
+            var result = Database.api.FirstOrDefault(x => x.cityName == selected_City);
+            string muwaqqit_URL = string.Empty;
+  
+            if (result == null)
             {
-                case "cph":
-                    ctx = ctx_cph;
-                    muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.6759142&ln=12.5691285&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                    sessionString = "PrayerData_cph";
-                    break;
-                case "odense":
-                    ctx = ctx_odense;
-                    muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.4037560&ln=10.4023700&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                    sessionString = "PrayerData_odense";
-                    break;
-                case "aarhus":
-                    ctx = ctx_aarhus;
-                    muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=56.1629390&ln=10.2039210&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                    sessionString = "PrayerData_aarhus";
-                    break;
-                case "aalborg":
-                    ctx = ctx_aalborg;
-                    muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=57.0488195&ln=9.9217470&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                    sessionString = "PrayerData_aalborg";
-                    break;
-                default:
-                    ctx = ctx_cph;
-                    muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.6759142&ln=12.5691285&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                    sessionString = "PrayerData_cph";
-                    break;
+                result = new APIResult();
+                result.cityName = selected_City;
+                switch (selected_City)
+                {
+                    case "cph":
+                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.6759142&ln=12.5691285&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
+                        //sessionString = "PrayerData_cph";
+                        break;
+                    case "odense":
+                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.4037560&ln=10.4023700&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
+                        //sessionString = "PrayerData_odense";
+                        break;
+                    case "aarhus":
+                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=56.1629390&ln=10.2039210&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
+                        //sessionString = "PrayerData_aarhus";
+                        break;
+                    case "aalborg":
+                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=57.0488195&ln=9.9217470&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
+                        //sessionString = "PrayerData_aalborg";
+                        break;
+                    default:
+                        result.cityName = "cph";
+                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.6759142&ln=12.5691285&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
+                        //sessionString = "PrayerData_cph";
+                        break;
+                }
             }
 
-            if (ctx == null ||
-                !JsonConvert.DeserializeObject<Root>(ctx).list.Any(n => n.fajr_date == getTodayDate()))
+            if (result.content == null ||
+                !JsonConvert.DeserializeObject<Root>(result.content).list.Any(n => n.fajr_date == getTodayDate()))
             {
 
                 using (var httpClient = new HttpClient())
@@ -69,14 +60,15 @@ namespace PrayerTimes.Controllers
                     using (var response = await httpClient.GetAsync(muwaqqit_URL))
                     {
                         string apiResponse = await response.Content.ReadAsStringAsync();
-                        HttpContext.Session.SetString(sessionString, apiResponse);
-                        ctx = HttpContext.Session.GetString(sessionString);
+                        result.content = apiResponse;
                     }
                 }
             }
-               
-            ViewData["prayers"] = JsonConvert.DeserializeObject<Root>(ctx).list;
 
+            ViewData["prayers"] = JsonConvert.DeserializeObject<Root>(result.content).list;
+            var findcity = Database.api.FirstOrDefault(x => x.cityName == result.cityName);
+            if(findcity==null)
+                Database.api.Add(result);
             return View();
         }
 
@@ -88,11 +80,6 @@ namespace PrayerTimes.Controllers
             }
 
             return RedirectToAction("Index");
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
