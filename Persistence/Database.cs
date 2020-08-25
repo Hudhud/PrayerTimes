a1fb198e13df02ad36fe98df366248358cc46ffc
Hadi Horani <hadihorani@hotmail.com>
2020-08-25 19:22:37 +0200

    db context fix

---
+++ Persistence/Database.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PrayerTimes.Persistence
+{
+
+    public class APIResult
+    {
+        public string cityName { get; set; }
+        public string content { get; set; }
+    }
+    public static class Database
+    {
+        public static List<APIResult> api { get; set; }
+    }
+}
