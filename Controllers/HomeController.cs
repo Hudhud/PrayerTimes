55471ddae081be6b982589fcbe6a7e7ef7737617
Hadi Horani <hadihorani@hotmail.com>
2020-09-28 02:40:07 +0200

    Modified to utilize a singleton instead of static variables, since it did not keep the prayer times data. The singleton approach fixed the issue

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,103 +16,40 @@ namespace PrayerTimes.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger _logger;
+        private readonly Database _database;
         static string selected_City;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, Database database)
         {
             _logger = logger;
+            _database = database;
         }
 
         public async Task<IActionResult> Index()
         {
-            //      string userIP = HttpContext.Connection.RemoteIpAddress.ToString();
-            //    _logger.LogInformation("Current user IP: " + userIP);
-            //      _logger.LogInformation("Previous user IP: " + Database.remoteAddress);
-
             if (HttpContext.Session.GetString("SameSession") == string.Empty || HttpContext.Session.GetString("SameSession") == null)
             {
                 HttpContext.Session.SetString("Active", "cph");
                 selected_City = "cph";
-                _logger.LogInformation("Active variable set to: " + HttpContext.Session.GetString("Active"));
                 HttpContext.Session.SetString("SameSession", "true");
             }
 
-            //if (HttpContext.Connection.RemoteIpAddress.ToString() != Database.remoteAddress)
-            //{
-            //    HttpContext.Session.SetString("Active", "cph");
-            //    selected_City = "cph";
-            //    _logger.LogInformation("Active variable set to: " + HttpContext.Session.GetString("Active"));
-            //    Database.remoteAddress = userIP;
-            //}
-
-            if (Database.api == null)
-                Database.api = new List<APIResult>();
-            var result = Database.api.FirstOrDefault(x => x.cityName == selected_City);
-            _logger.LogInformation("Database result:");
-
-            string muwaqqit_URL = string.Empty;
-
-            if (result == null)
+            if (string.IsNullOrEmpty(selected_City))
             {
-                result = new APIResult();
-                result.cityName = selected_City;
-                switch (selected_City)
-                {
-                    case "cph":
-                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.6759142&ln=12.5691285&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                        break;
-                    case "odense":
-                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.4037560&ln=10.4023700&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                        break;
-                    case "aarhus":
-                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=56.1629390&ln=10.2039210&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                        break;
-                    case "aalborg":
-                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=57.0488195&ln=9.9217470&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                        break;
-                    default:
-                        result.cityName = "cph";
-                        muwaqqit_URL = "https://www.muwaqqit.com/api.json?lt=55.6759142&ln=12.5691285&d=" + getTodayDate() + "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
-                        HttpContext.Session.SetString("Active", result.cityName);
-                        break;
-                }
-            } else
-            {
-                _logger.LogInformation(result.content);
+                selected_City = "cph";
             }
 
             try
             {
-                if (result.content == null ||
-                              !JsonConvert.DeserializeObject<Root>(result.content).list.Any(n => n.fajr_date == getTodayDate()))
-                {
-                    if (result.content != null && !JsonConvert.DeserializeObject<Root>(result.content).list.Any(n => n.fajr_date == getTodayDate()))
-                        Database.api = new List<APIResult>();
-
-                    using (var httpClient = new HttpClient())
-                    {
-                        using (var response = await httpClient.GetAsync(muwaqqit_URL))
-                        {
-                            string apiResponse = await response.Content.ReadAsStringAsync();
-                            result.content = apiResponse;
-                        }
-                    }
-                }
-
-                ViewData["prayers"] = JsonConvert.DeserializeObject<Root>(result.content).list;
-                var findcity = Database.api.FirstOrDefault(x => x.cityName == result.cityName);
-
-                if (findcity == null)
-                    Database.api.Add(result);
+                var content = await _database.GetApiResult(selected_City.ToLower());
+                ViewData["prayers"] = JsonConvert.DeserializeObject<Root>(content).list;
             }
             catch (Exception e)
             {
-                _logger.LogInformation("ERROR IS HERE");
                 _logger.LogInformation(e.Message);
-                _logger.LogInformation(e.StackTrace);
             }
 
-            
+
             ViewData["Active"] = HttpContext.Session.GetString("Active");
             ViewData["Deactive"] = HttpContext.Session.GetString("Deactive");
 
@@ -121,10 +58,12 @@ namespace PrayerTimes.Controllers
 
         public IActionResult check(string button_value)
         {
-            if (!button_value.Equals("cph")) { 
+            if (!button_value.Equals("cph"))
+            {
                 ViewData["Deactive"] = "cph";
                 HttpContext.Session.SetString("Deactive", "cph");
-            } else
+            }
+            else
             {
                 ViewData["Deactive"] = null;
                 HttpContext.Session.SetString("Deactive", string.Empty);
@@ -144,13 +83,6 @@ namespace PrayerTimes.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public string getTodayDate()
-        {
-            DateTime dt = DateTime.Today;
-            string dateFormatted = dt.Date.ToString("yyyy-MM-dd");
-            return dateFormatted;
         }
     }
 }
--- Persistence/Database.cs
+++ Persistence/Database.cs
@@ -1,16 +1,90 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using PrayerTimes.Models;
 
 namespace PrayerTimes.Persistence
 {
-
     public class APIResult
     {
         public string cityName { get; set; }
         public string content { get; set; }
+        public string url { get; set; }
     }
-    public static class Database
+
+    public class Database
     {
-        public static List<APIResult> api { get; set; }
-        public static string remoteAddress { get; set; }
+        private const string URL_SUFFIX = "&tz=Europe%2FCopenhagen&fa=-18.0&ea=-17.0&fea=0&rsa=0";
+        private readonly Dictionary<string, APIResult> api;
+
+        public Database()
+        {
+            var cph = new APIResult
+            {
+                cityName = "cph",
+                url = "https://www.muwaqqit.com/api.json?lt=55.6759142&ln=12.5691285&d="
+            };
+
+            var odense = new APIResult
+            {
+                cityName = "odense",
+                url = "https://www.muwaqqit.com/api.json?lt=55.4037560&ln=10.4023700&d="
+            };
+
+            var aarhus = new APIResult
+            {
+                cityName = "aarhus",
+                url = "https://www.muwaqqit.com/api.json?lt=56.1629390&ln=10.2039210&d="
+            };
+
+            var aalborg = new APIResult
+            {
+                cityName = "aalborg",
+                url = "https://www.muwaqqit.com/api.json?lt=57.0488195&ln=9.9217470&d="
+            };
+
+            api = new Dictionary<string, APIResult>
+            {
+                { cph.cityName, cph },
+                { odense.cityName, odense },
+                { aarhus.cityName, aarhus },
+                { aalborg.cityName, aalborg }
+            };
+        }
+
+        public async Task<string> GetApiResult(string cityName)
+        {
+            var apiResult = api[cityName.ToLower()];
+            if (apiResult.content == null || !JsonConvert.DeserializeObject<Root>(apiResult.content).list.Any(n => n.fajr_date == getTodayDate()))
+            {
+                var url = $"{apiResult.url}{getTodayDate()}{URL_SUFFIX}";
+                var content = await GetApiContent(url);
+                apiResult.content = content;
+            }
+
+            return apiResult.content;
+        }
+
+        private async Task<string> GetApiContent(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(url))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return apiResponse;
+                }
+            }
+        }
+
+        private string getTodayDate()
+        {
+            DateTime dt = DateTime.Today;
+            string dateFormatted = dt.Date.ToString("yyyy-MM-dd");
+            return dateFormatted;
+        }
     }
 }
