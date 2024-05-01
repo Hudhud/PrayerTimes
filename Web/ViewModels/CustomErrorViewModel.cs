e12c6e51b7c2f30ae3ed2db12b8b545b13ebcd2e
Hadi Horani <hadihorani@hotmail.com>
2024-05-01 23:55:28 +0200

    improve

--- Web/ViewModels/CustomErrorViewModel.cs
+++ Web/ViewModels/CustomErrorViewModel.cs
@@ -5,6 +5,7 @@
         public string RequestId { get; set; }
         public string ErrorMessage { get; set; }
         public string ErrorCode { get; set; }
+        public string Details { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
