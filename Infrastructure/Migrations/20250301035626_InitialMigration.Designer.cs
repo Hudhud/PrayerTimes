53afaca59e829fafb3eb8ad904ea9931e54c9229
Hadi Horani <hadihorani@hotmail.com>
2025-03-01 05:00:59 +0100

    Rollback

--- Infrastructure/Migrations/20250301035626_InitialMigration.Designer.cs
+++ Infrastructure/Migrations/20250301035626_InitialMigration.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240904003604_InitialMigration")]
+    [Migration("20250301035626_InitialMigration")]
     partial class InitialMigration
     {
         /// <inheritdoc />
